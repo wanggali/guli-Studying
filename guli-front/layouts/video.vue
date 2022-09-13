4c609d29f8cf3288fcb9b55b42e0d3a6a00ae5aa
gali <2722933638@qq.com>
2022-09-13 21:54:27 +0800

    前台登录，注册页面修改

--- guli-front/layouts/video.vue
+++ guli-front/layouts/video.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="guli-player">
     <div class="head">
-      <a href="#" title="谷粒学院">
-        <img class="logo" src="~/assets/img/logo.png" lt="谷粒学院">
+      <a href="#" title="谷粒学苑">
+        <img class="logo" src="~/assets/img/logo.png" lt="谷粒学苑">
     </a></div>
     <div class="body">
       <div class="content"><nuxt/></div>
--- guli-front/nuxt.config.js
+++ guli-front/nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ** Headers of the page
   */
  head: {
-  title: '谷粒学院 - Java视频|HTML5视频|前端视频|Python视频|大数据视频-自学拿1万+月薪的IT在线视频课程，谷粉力挺，老学员为你推荐',
+  title: '谷粒学苑 - Java视频|HTML5视频|前端视频|Python视频|大数据视频-自学拿1万+月薪的IT在线视频课程，谷粉力挺，老学员为你推荐',
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
